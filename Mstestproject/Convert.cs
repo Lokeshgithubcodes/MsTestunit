edd65c36a3b8f5465ac946177baf072f58a1463a
Lokeshgithubcodes <ramisettylokesh642@gmail.com>
2024-02-09 20:32:21 +0530

    first commit

---
+++ Mstestproject/Convert.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mstestproject
+{
+    public static class Convert
+    {
+        public static int cal(int a,int b)
+        {
+            return a + b;
+        }
+
+        public static int sub(int a,int b)
+        {
+            return a - b;
+        }
+
+        public static int mul(int a,int b)
+        {
+            return a * b;
+        }
+
+        public static double div(double a,double b)
+        {
+            return a / b;
+        }
+
+        public static string name(string s)
+        {
+            return s;
+        }
+    }
+}
---
+++ MstestprojectTests/ConvertTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mstestproject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mstestproject.Tests
+{
+    [TestClass()]
+    public class ConvertTests
+    {
+        [TestMethod()]
+        public void cal()
+        {
+            int a = 10;
+            int b = 20;
+            int res=Convert.cal(a, b);
+            Assert.AreEqual(30, res);
+
+        }
+
+        [TestMethod()]
+        public void sub() 
+        {
+            int a = 20;
+            int b = 20;
+            int res1=Convert.sub(a, b);
+            Assert.AreEqual(0, res1);
+        }
+        [TestMethod()]
+        public void mul()
+        {
+            int a = 3;
+            int b=3;
+            int res3 = Convert.mul(a, b);
+            Assert.AreEqual(9, res3);
+        }
+        [TestMethod()]
+        public void div()
+        {
+            double a = 3;
+            double b = 3;
+            double res4= Convert.div(a, b);
+            Assert.AreEqual(1, res4);
+
+        }
+        
+        //[TestMethod()]
+
+        //public void name()
+        //{
+        //    string s = "Lokesh";
+        //    string res5= Convert.name(s);
+        //    Assert.AreEqual("Lokesh", res5);
+        //}
+
+       
+    }
+}
